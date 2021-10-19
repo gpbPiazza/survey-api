8b9079d1be016c4ff35f71a95b6bc5e8ad67f562
Gabriel <gabriel.piazza@elevaeducacao.com.br>
2021-10-19 08:15:13 -0300

    refactor: add costumized error for missing param

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,4 +1,5 @@
-import { SignUpController } from "./signupController";
+import { MissingParamError } from "../errors/missing-param-error";
+import { SignUpController } from "./signup";
 
 describe('SignUp Controller', () => {
 	test('Should return  400 if no name is provided ', () => {
@@ -15,7 +16,7 @@ describe('SignUp Controller', () => {
 		const httpResponse = signUp.handle(httpRequest)
 
 		expect(httpResponse.statusCode).toBe(400)
-		expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+		expect(httpResponse.body).toEqual(new MissingParamError('name'))
 	});
 
 	test('Should return  400 if no email is provided ', () => {
@@ -32,6 +33,6 @@ describe('SignUp Controller', () => {
 		const httpResponse = signUp.handle(httpRequest)
 
 		expect(httpResponse.statusCode).toBe(400)
-		expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+		expect(httpResponse.body).toEqual(new MissingParamError('email'))
 	});
 });
