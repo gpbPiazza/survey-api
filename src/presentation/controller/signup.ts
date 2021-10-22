165b3509583dd4d8bca6dc8ed3505f0dc51b002f
Gabriel <gabriel.piazza@elevaeducacao.com.br>
2021-10-22 08:19:44 -0300

    refactor: move all error imports to a single file

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,6 +1,4 @@
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { MissingParamError } from '../errors/missing-param-error'
-import { ServerError } from '../errors/server-error'
+import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
