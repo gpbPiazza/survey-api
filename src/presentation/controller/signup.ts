1860bdf020ab25cf15c116b65007aee2def70bab
Gabriel <gabriel.piazza@elevaeducacao.com.br>
2021-10-22 08:15:07 -0300

    refactor: create serverError Helper

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,7 +1,7 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
 import { ServerError } from '../errors/server-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/https'
@@ -23,10 +23,7 @@ export class SignUpController implements Controller {
       const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValidEmail) { return badRequest(new InvalidParamError('email')) }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
