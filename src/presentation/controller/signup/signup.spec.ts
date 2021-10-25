3c417c4bcf6e067d9bab2fa7f70844ab1ce814d9
Gabriel <gabriel.piazza@elevaeducacao.com.br>
2021-10-25 07:11:09 -0300

    test: ensure SignUpController return 200 if valida data is provided

--- src/presentation/controller/signup/signup.spec.ts
+++ src/presentation/controller/signup/signup.spec.ts
@@ -233,4 +233,27 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Should return 200 if valid data is provided', () => {
+    const { singUpController } = makeSignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+    const httpResponse = singUpController.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(200)
+
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    })
+  })
 })
