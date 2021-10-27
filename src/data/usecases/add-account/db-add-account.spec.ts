29e0ba33c887cfbae1505dd984f8b375bfd74bb0
Gabriel <gabriel.piazza@elevaeducacao.com.br>
2021-10-27 07:29:27 -0300

    refactor: move DbAddAccount creation to a factory mathod

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,16 +1,30 @@
+import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
+
+interface MakeTypes {
+  dbAddAccount: DbAddAccount
+  encrypter: Encrypter
+}
+
+const makeDbAddAccount = (): MakeTypes => {
+  class Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+
+  const encrypter = new Encrypter()
+
+  const dbAddAccount = new DbAddAccount(encrypter)
+  return {
+    encrypter,
+    dbAddAccount
+  }
+}
 
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class Encrypter {
-      async encrypt (value: string): Promise<string> {
-        return await new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-
-    const encrypter = new Encrypter()
-
-    const dbAddAccount = new DbAddAccount(encrypter)
+    const { encrypter, dbAddAccount } = makeDbAddAccount()
 
     const encrypterSpy = jest.spyOn(encrypter, 'encrypt')
 
