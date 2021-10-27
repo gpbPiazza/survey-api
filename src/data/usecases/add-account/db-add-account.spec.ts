fb3f0b74e2d8021ae20a9049888f5e31bcc3807d
Gabriel <gabriel.piazza@elevaeducacao.com.br>
2021-10-27 07:30:47 -0300

    refactor: move Encrypter creation to a factory mathod

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -6,15 +6,18 @@ interface MakeTypes {
   encrypter: Encrypter
 }
 
-const makeDbAddAccount = (): MakeTypes => {
+const makeEncrypter = (): Encrypter => {
   class Encrypter {
     async encrypt (value: string): Promise<string> {
       return await new Promise(resolve => resolve('hashed_password'))
     }
   }
 
-  const encrypter = new Encrypter()
+  return new Encrypter()
+}
 
+const makeDbAddAccount = (): MakeTypes => {
+  const encrypter = makeEncrypter()
   const dbAddAccount = new DbAddAccount(encrypter)
   return {
     encrypter,
