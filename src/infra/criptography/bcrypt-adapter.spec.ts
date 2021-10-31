ee6662053bd6bfa3009f7cd8eec5830e67df4942
Gabriel <gabriel.piazza@elevaeducacao.com.br>
2021-10-31 11:45:27 -0300

    test: ensure bcrypt will be called with correct values

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,15 @@
+import bcrypt from 'bcrypt'
+import {BcryptAdapter} from './bcrypt-adapter'
+
 describe('Bcrypt Adapter', () => {
   test('Should call Bcrypt with correct values', async () => {
-    const bcryptAdapter = new BcryptAdapter()
+    const salt = 12
+    const bcryptAdapter = new BcryptAdapter(salt)
+
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+
     await bcryptAdapter.encrypt('any_value')
+
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 })
