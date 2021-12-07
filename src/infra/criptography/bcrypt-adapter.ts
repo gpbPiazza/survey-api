c15fc0149a0e0c153134c9e633d3692264bd7967
gpbPiazza <pedro.piazza99@gmail.com>
2021-12-07 08:00:09 -0300

    feat: ensure BcryptAdapter return a hasn on Success

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -9,8 +9,8 @@ export class BcryptAdapter implements Encrypter {
   }
 
   async encrypt (value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt)
+    const hash = await bcrypt.hash(value, this.salt)
 
-    return ''
+    return hash
   }
 }
