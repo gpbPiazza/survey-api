df586ff4e95e7399afbc87303f1368b5ae09422c
gpbPiazza <pedro.piazza99@gmail.com>
2022-03-02 16:45:03 -0300

    refactor: ensure email field in LoginController is required

---
+++ src/presentation/controller/login/login.ts
@@ -0,0 +1,14 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class LoginController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { body } = httpRequest
+    const requiredFields = ['email']
+
+    for (const field of requiredFields) {
+      if (!body[field]) { return badRequest(new MissingParamError(field)) }
+    }
+  }
+}
