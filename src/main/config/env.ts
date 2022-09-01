aa132eca7f316a2cf28d3c4e3e23e7b48279e1ab
Gabriel Piazza <pedro.piazza99@gmail.com>
2022-09-01 08:38:38 -0300

    chore: add docker-compose'

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUlr: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUlr: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT || 5050,
   saltEncrypt: 12,
   jwtSecrect: process.env.JWT_SECRECT || 'TJ670==5hw'
