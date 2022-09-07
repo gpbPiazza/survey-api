302d86eb16759a3525bb38b76d0e03cc5d494900
Gabriel Piazza <pedro.piazza99@gmail.com>
2022-09-07 13:45:56 -0300

    refactor: move composition of makeLogControllerDecorator to a factory

--- src/main/factories/controllers/login/login-factory.ts
+++ src/main/factories/controllers/login/login-factory.ts
@@ -1,14 +1,11 @@
 import { Controller } from '../../../../presentation/protocols'
-import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log-repository/log-mongo-repository'
 import { LoginController } from '../../../../presentation/controller/login/login-controller'
 import { makeLoginValidation } from './login-validation-factory'
 import { makeAuthentication } from '../../usecases/db-authentication-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 
 export const makeLoginController = (): Controller => {
-  const logMongoRepository = new LogMongoRepository()
-
   const loginController = new LoginController(makeLoginValidation(), makeAuthentication())
 
-  return new LogControllerDecorator(loginController, logMongoRepository)
+  return makeLogControllerDecorator(loginController)
 }
--- src/main/factories/controllers/signup/signup-factory.ts
+++ src/main/factories/controllers/signup/signup-factory.ts
@@ -1,15 +1,12 @@
 import { SignUpController } from '../../../../presentation/controller/signup/signup-controller'
 import { Controller } from '../../../../presentation/protocols'
-import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log-repository/log-mongo-repository'
 import { makeSignupValidation } from './signup-validation-factory'
 import { makeAuthentication } from '../../usecases/db-authentication-factory'
 import { makeAddAccount } from '../../usecases/db-add-account-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 
 export const makeSignupController = (): Controller => {
-  const logMongoRepository = new LogMongoRepository()
-
   const signUpController = new SignUpController(makeAddAccount(), makeSignupValidation(), makeAuthentication())
 
-  return new LogControllerDecorator(signUpController, logMongoRepository)
+  return makeLogControllerDecorator(signUpController)
 }
