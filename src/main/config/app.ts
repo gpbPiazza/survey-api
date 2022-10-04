a98f261a2bd01e210f16896b702767c16bb15adf
Gabriel Piazza <pedro.piazza99@gmail.com>
2022-10-04 07:43:21 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setUpRoutes from './routes'
+import setupAPIDoc from './config-swagger'
 
 const app = express()
-
+setupAPIDoc(app)
 setupMiddlewares(app)
 setUpRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Survey API',
+    description: 'Survey API is system provides admin users to create surveys with answers and common users to answer then',
+    version: ''
+  }
+}
