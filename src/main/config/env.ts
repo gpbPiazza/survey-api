a1c0eb1ee454b38c0eedd5cd9d123988079f7ada
Gabriel Piazza <pedro.piazza99@gmail.com>
2022-10-05 08:56:43 -0300

    refactor: rename project

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUlr: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUlr: process.env.MONGO_URL || 'mongodb://localhost:27017/survey-node-api',
   port: process.env.PORT || 5050,
   saltEncrypt: 12,
   jwtSecrect: process.env.JWT_SECRECT || 'TJ670==5hw'
